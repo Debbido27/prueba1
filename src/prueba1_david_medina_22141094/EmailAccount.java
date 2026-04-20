99c4063858d880e222bfd50112e6ec6dc9221c0f
Debbido27 <davidmedina272004@gmail.com>
2026-04-20 15:24:02 -0600

    Clase Email Account, con los atributos de direccion contrasena, nombre completo, inbox

--- src/prueba1_david_medina_22141094/EmailAccount.java
+++ src/prueba1_david_medina_22141094/EmailAccount.java
@@ -1,13 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package prueba1_david_medina_22141094;
 
-/**
- *
- * @author Dell
- */
 public class EmailAccount {
+    String address;
+    String password;
+    String nombreCompleto;
+    Email [] inbox;
+   
+    public EmailAccount(String address, String password, String nombreCompleto){
+        this.address=address;
+        this.password=password;
+        this.nombreCompleto=nombreCompleto;
+        this.inbox=new Email[100];
+    }
+    
+    
     
 }
