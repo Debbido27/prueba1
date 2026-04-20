2fecf7554872081e14943d303438cd29a227ea52
Debbido27 <davidmedina272004@gmail.com>
2026-04-20 15:18:39 -0600

    Clase Email terminada, con el metodo para marcar Leido, y e metodo para impresion

--- src/prueba1_david_medina_22141094/Email.java
+++ src/prueba1_david_medina_22141094/Email.java
@@ -4,10 +4,65 @@
  */
 package prueba1_david_medina_22141094;
 
-/**
- *
- * @author Dell
- */
 public class Email {
+String correoE;
+String asunto;
+String contenido;
+boolean leido;
+
+
+public Email (String correoE, String asunto, String contenido){
+    this.correoE=correoE;
+    this.asunto=asunto;
+    this.contenido=contenido;
+    this.leido=false;
+}
+
+    public String getCorreoE() {
+        return correoE;
+    }
+
+    public void setCorreoE(String correoE) {
+        this.correoE = correoE;
+    }
+
+    public String getAsunto() {
+        return asunto;
+    }
+
+    public void setAsunto(String asunto) {
+        this.asunto = asunto;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public boolean isLeido() {
+        return leido;
+    }
+
+    public void setLeido(boolean leido) {
+        this.leido = leido;
+    }
+
+    
+    public void marcarLeido(){
+        this.leido=true;
+    }
+
+    
+    public void impresion(){
+        System.out.print("De: <"+correoE+">");
+        System.out.print("ASUNTO: <"+asunto+">");
+        System.out.println("<"+contenido+">");
+        
+    }
+
     
 }
+
